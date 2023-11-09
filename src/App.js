d1520341884c88cbd0976798c9f642ffdb0f1910
VERA-OBIERO <vera.obiero@gmail.com>
2023-11-09 22:13:02 +0300

    add create form component for users to add their own events

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Header from './components/Header'
 import EventList from './components/EventList'
+import AddEventForm from './components/AddEventForm'
 
 function App() {
   return (
     <ChakraProvider>
       <Header />
+      <AddEventForm />
       <EventList />
     </ChakraProvider>
   )
